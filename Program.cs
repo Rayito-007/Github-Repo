0c8854d0fd95f4854bacd6202a18a36f4270d4d6
Jarrker <130817601+Jarrker@users.noreply.github.com>
2024-05-15 00:38:58 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,7 +10,15 @@ namespace Github_Repo
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("HOLA upn")
+            Console.WriteLine("¡Hola! ¿Cuál es tu nombre?");
+            string nombre = Console.ReadLine(); // Pedir al usuario que ingrese su nombre
+
+            // Mostrar un saludo utilizando el nombre ingresado
+            Console.WriteLine($"¡Hola, {nombre}! Bienvenido.");
+
+            // Esperar a que el usuario presione una tecla antes de cerrar la aplicación
+            Console.WriteLine("Presiona cualquier tecla para salir...");
+            Console.ReadKey();
         }
     }
 }
